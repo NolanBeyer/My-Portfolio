1b1fc80f38cd6ff1eed01d4cfa13f67a0cf9a8b1
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2020-11-06 02:57:55 -0800

    touched up contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,10 +6,9 @@ const Contact = () => {
 			<div className='contact-main'>
 				<h1>Get in touch with me!</h1>
 				<h2 className='info'>EMAIL: nolanbeyer40@gmail.com</h2>
-				<h2 className='info'>PHONE: (541) 979-7040</h2>
 				<h2 className='info'>FACEBOOK: <a className='links' href='https://www.facebook.com/nolan.beyer/'><i class="fab fa-facebook"></i></a></h2>
 				<h2 className='info'>
-					LINKEDIN: <a className='links' href='https://www.linkedin.com/in/nolan-beyer-5252731b1/' className='fab fa-linkedin'>Linked in</a>
+					LINKEDIN: <a className='links' href='https://www.linkedin.com/in/nolan-beyer-5252731b1/' className='fab fa-linkedin'></a>
 				</h2>
 				<h2 className='info'>
 					GITHUB :
