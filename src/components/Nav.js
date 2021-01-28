d783056e356e027c4db8f844133fb361e7586f79
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-01-28 05:10:22 -0800

    refacored and switched nav from state to a functional component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,10 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import './Nav.css'
 
-class Nav extends React.Component {
-	constructor() {
-		super()
-
-		this.state = {
-			dropdown: false,
-		}
-	}
-
-	toggleDropDown = () => {
-		this.setState({ dropdown: !this.state.dropdown })
-	}
-
-	render() {
+const Nav = () => {
 		return (
-			<div>
+			<Fragment>
 				<div className='main--nav'>
 					<Link className='links' to={'/'}>
 						Home
@@ -29,27 +16,9 @@ class Nav extends React.Component {
 						Contact
 					</Link>
 				</div>
-
-				<div className='dropdown' onClick={this.toggleDropDown}>
-					<i class='fas fa-bars burger'></i>
-					</div>
-				{this.state.dropdown ? (
-					<div className='mobile-menu'>
-						<Link className='links' to={'/'}>
-							Home
-						</Link>
-						<Link className='links' to={'/about'}>
-							About
-						</Link>
-						<Link className='links' to={'contact'}>
-							Contact
-						</Link>
-					</div>
-				) : null}
-			</div>
+			</Fragment>
 			
 		)
 	}
-}
 
 export default Nav
