c5f9c7d07ff2f038e36df79c8770be2f5d741704
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-02-25 02:36:10 -0800

    re formated code and deleted un needed use History

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import {useHistory} from 'react-router-dom'
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-    return(
-        <div className='main-footer'>
-            <a className='links' href='https://github.com/NolanBeyer'><i class="fab fa-github"></i> GitHub</a>
-            <a className='links' href='https://www.linkedin.com/in/nolan-beyer-5252731b1/'><i class="fab fa-linkedin"></i> in LinkedIn</a>
-            <a className='links' href="nolanbeyer40@gmail.com"><i class="fas fa-envelope"></i> Email</a>
-        </div>
-    )
-}
+  return (
+    <div className="main-footer">
+      <a className="links" href="https://github.com/NolanBeyer">
+        <i class="fab fa-github"></i> GitHub
+      </a>
+      <a
+        className="links"
+        href="https://www.linkedin.com/in/nolan-beyer-5252731b1/">
+        <i class="fab fa-linkedin"></i> in LinkedIn
+      </a>
+      <a className="links" href="nolanbeyer40@gmail.com">
+        <i class="fas fa-envelope"></i> Email
+      </a>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
