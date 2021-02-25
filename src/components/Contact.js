365217360a38615d0f4f594397cf1cc51f607d93
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-02-25 02:35:23 -0800

    Updated contact links and addressed all warnings

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
-	return (
-		<div className='contact-body'>
-			<div className='contact-main'>
-				<h1>Get in touch with me!</h1>
-				<h2 className='info'>EMAIL: nolanbeyer40@gmail.com</h2>
-				<h2 className='info'>FACEBOOK: <a className='links' href='https://www.facebook.com/nolan.beyer/'><i class="fab fa-facebook"></i></a></h2>
-				<h2 className='info'>
-					LINKEDIN: <a className='links' href='https://www.linkedin.com/in/nolan-beyer-5252731b1/' className='fab fa-linkedin'></a>
-				</h2>
-				<h2 className='info'>
-					GITHUB :
-					<a className='links' className='fab fa-github' href='https://github.com/NolanBeyer'></a>
-				</h2>
-				<h2 className='info'>
-					RESUME: <a className='links' href='https://docs.google.com/document/d/1vDKrCI6gJ_dlaeLlpHgoYI028fGBEmxCJwByaa39pTw/edit?usp=sharing'><i class="far fa-file"></i></a>
-				</h2>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="contact">
+      <div className="contact__me">
+        <h1 className="contact__title">Get in touch with me!</h1>
+        <h2 className="contact__info">EMAIL: nolanbeyer40@gmail.com</h2>
+        <h2 className="contact__info">
+          FACEBOOK:{" "}
+          <a
+            className="contact__links fab fa-facebook"
+            href="https://www.facebook.com/nolan.beyer/"> </a>
+        </h2>
+        <h2 className="contact__info">
+          LINKEDIN:{" "}
+          <a
+            className="contact__links fab fa-linkedin"
+            href="https://www.linkedin.com/in/nolan-beyer-5252731b1/"> </a>
+        </h2>
+        <h2 className="contact__info">
+          GITHUB: {" "}
+          <a
+            className="contact__links fab fa-github"
+            href="https://github.com/NolanBeyer"> </a>
+        </h2>
+        <h2 className="contact__info">
+          RESUME:{" "}
+          <a
+            className="contact__links far fa-file"
+            href="https://docs.google.com/document/d/1vDKrCI6gJ_dlaeLlpHgoYI028fGBEmxCJwByaa39pTw/edit?usp=sharing"> </a>
+        </h2>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
