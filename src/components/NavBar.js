1226e1d5427b6b7b2561cc55652a39394861ef7d
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-02-25 02:36:48 -0800

    Updated home logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,48 +1,46 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { useHistory } from "react-router";
-import './NavBar.css'
+import "./NavBar.css";
 
 const NavBar = () => {
   const history = useHistory();
   return (
     <>
-      <Navbar className="NavBar" fixed="top" collapseOnSelect expand="lg" variant="dark">
+      <Navbar
+        className="NavBar"
+        fixed="top"
+        collapseOnSelect
+        expand="lg"
+        variant="dark">
         <Navbar.Brand
+          className="home__icon"
           style={{ cursor: "pointer" }}
           onClick={() => {
             history.push("/");
-          }}
-        >
+          }}>
           {" "}
-          <img
-            width="50"
-            height="50"
-            alt=""
-            src="../../../../IMG_8501-2 (wecompress.com) (1).jpeg"
-          ></img>
+          <i class="home-icon fas fa-home"></i>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto"></Nav>
 
-          <Nav.Link className="links"
-               onClick={() => {
-                history.push("/about");
-              }}
-          >
+          <Nav.Link
+            className="links"
+            onClick={() => {
+              history.push("/about");
+            }}>
             About
           </Nav.Link>
 
-          <Nav.Link className="links"
-              onClick={() => {
-                history.push("/contact");
-              }}
-          >
+          <Nav.Link
+            className="links"
+            onClick={() => {
+              history.push("/contact");
+            }}>
             Contact
           </Nav.Link>
-
-          
         </Navbar.Collapse>
       </Navbar>
     </>
