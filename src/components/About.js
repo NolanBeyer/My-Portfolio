d1fc9383ddbf9de161cf1914a5139e98b189282e
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-02-25 02:34:33 -0800

    Fixed padding and margin issues

--- src/components/About.js
+++ src/components/About.js
@@ -1,41 +1,51 @@
-import React from 'react';
-import './About.css';
+import React from "react";
+import "./About.css";
 
 const About = () => {
-	return (
-		<div className='main'>
-			<div className='about'>
-				<h1>About Me</h1>
-				<h4>I've always been really interested in technology, but never thought
-				about a career in web development until summer of 2019. I started doing
-				some research and just trying to learn some of the basics in HTML and
-				CSS. I fell in love with the thought of making this a full time career.
-				I've spent countless hours over the last year and although I still have
-				a lot to learn I'm really excited for this next step and am willing to
-				learn additional technologies to fit the needs of future employers.
-				</h4>
-				<div className='jobs'>
-					<h1>Experience</h1>
-					<div clasName='job1'>
-						<h2>Dev Mountain</h2>
-						<h4>Immersive, 13-week web-development bootcamp for learning Javascript, HTML/CSS, React, Node, and PostgreSQL frameworks.</h4>
-					</div>
-			<div className='jobs'>
-				<div className='job3'>
-					<h2>REdevly</h2>
-					<h4>Working part time for a startup primarily focusing on HTML, CSS/SCSS along with learning the basics of angular. </h4>
-				</div>
-			</div>
-			</div>
-			<div className='jobs'>
-				<div className='job2'>
-					<h2>Graphic designer</h2>
-					<h4>Designed graphics for various buisnesess using adobe illustrator.</h4>
-				</div>
-			</div>
-		</div>
-		</div>
-	)
-}
+  return (
+    <div className="about">
+      <div className="about__body">
+        <h1 className="about__title">About Me</h1>
+        <h4 className="about__description">
+          I've always been really interested in technology, but never thought
+          about a career in web development until summer of 2019. I started
+          doing some research and just trying to learn some of the basics in
+          HTML and CSS. I fell in love with the thought of making this a full
+          time career. I've spent countless hours over the last year and
+          although I still have a lot to learn I'm really excited for this next
+          step and am willing to learn additional technologies to fit the needs
+          of future employers.
+        </h4>
+        <div className="work">
+          <h1 className="about__title">Experience</h1>
+          <div className="work__history">
+            <h2 className="work__title">Dev Mountain:</h2>
+            <h4>
+              Immersive, 13-week web-development bootcamp for learning
+              Javascript, HTML/CSS, React, Node, and PostgreSQL frameworks.
+            </h4>
+          </div>
+          <div className="work">
+            <h2 className="work__title">REdevly:</h2>
+            <div className="work__history">
+              <h4 className="work__description">
+                Working part time for a startup primarily focusing on HTML,
+                CSS/SCSS along with learning the basics of angular.{" "}
+              </h4>
+            </div>
+          </div>
+        </div>
+        <div className="work">
+          <div className="work__history">
+            <h2 className="work__title">Graphic designer:</h2>
+            <h4 className="work__description">
+              Designed graphics for various buisnesess using adobe illustrator.
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
