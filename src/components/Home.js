0d8e890bc7d5113cbb0bc3deaf5231ad1d35a9de
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-02-25 22:00:28 -0800

    Made about and contact pages responsive for mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,92 +20,94 @@ const Home = () => {
       </div>
 
       <div className="cards">
-        <Row>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src="https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-              />
-              <Card.Body>
-                <Card.Title className="text-center text-black">
-                  Offer Up
-                </Card.Title>
-                <hr />
-                <Card.Text>
-                  My first full stack app with a database and authentication.
-                </Card.Text>
-                <a
-                  className="buttons btn btn-dark"
-                  href="https://github.com/NolanBeyer/Task-Tracker">
-                  <i className="fab fa-chrome"> Site</i>
-                </a>
-                <a
-                  className="buttons btn btn-dark"
-                  href="https://github.com/NolanBeyer/personal-project">
-                  <i className="fab fa-github"> Code</i>
-                </a>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card
-              width="70px"
-              height="70px"
-              className="text-center"
-              style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src="https://images.unsplash.com/photo-1494869042583-f6c911f04b4c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-              />
-              <Card.Body>
-                <Card.Title className="text-black">Face Rec</Card.Title>
-                <hr />
-                <Card.Text>
-                  A two factor authentication site with face id to store all
-                  your passwords from various sites in.
-                </Card.Text>
-                <a
-                  className="buttons btn btn-dark"
-                  href="https://face-id.dev/#/">
-                  <i className="fab fa-chrome"> Site</i>
-                </a>
-                <a
-                  className="buttons btn btn-dark"
-                  href="https://github.com/NolanBeyer/image-recognition">
-                  <i className="fab fa-github"> Code</i>
-                </a>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card className="text-center" style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src="https://images.unsplash.com/photo-1489486501123-5c1af10d0be6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-              />
-              <Card.Body>
-                <Card.Title className="text-black">Task Tracker</Card.Title>
-                <hr />
-                <Card.Text>
-                  Add, complete, delete, set reminders and toggle completed
-                </Card.Text>
+          <Row>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img
+                  variant="top"
+                  src="https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+                />
+                <Card.Body>
+                  <Card.Title className="text-center text-black">
+                    Offer Up
+                  </Card.Title>
+                  <hr />
+                  <Card.Text>
+                    My first full stack app with a database and authentication.
+                  </Card.Text>
+                  <a
+                    className="buttons btn btn-dark"
+                    href="https://github.com/NolanBeyer/Task-Tracker">
+                    <i className="fab fa-chrome"> Site</i>
+                  </a>
+                  <a
+                    className="buttons btn btn-dark"
+                    href="https://github.com/NolanBeyer/personal-project">
+                    <i className="fab fa-github"> Code</i>
+                  </a>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card
+                width="70px"
+                height="70px"
+                className="text-center"
+                style={{ width: "18rem" }}>
+                <Card.Img
+                  variant="top"
+                  src="https://images.unsplash.com/photo-1494869042583-f6c911f04b4c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+                />
+                <Card.Body>
+                  <Card.Title className="text-black">Face Rec</Card.Title>
+                  <hr />
+                  <Card.Text>
+                    A two factor authentication site with face id to store all
+                    your passwords from various sites in.
+                  </Card.Text>
+                  <a
+                    className="buttons btn btn-dark"
+                    href="https://face-id.dev/#/">
+                    <i className="fab fa-chrome"> Site</i>
+                  </a>
+                  <a
+                    className="buttons btn btn-dark"
+                    href="https://github.com/NolanBeyer/image-recognition">
+                    <i className="fab fa-github"> Code</i>
+                  </a>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card
+                className="card-info text-center"
+                style={{ width: "18rem" }}>
+                <Card.Img
+                  variant="top"
+                  src="https://images.unsplash.com/photo-1489486501123-5c1af10d0be6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+                />
+                <Card.Body>
+                  <Card.Title className="text-black">Task Tracker</Card.Title>
+                  <hr />
+                  <Card.Text>
+                    Add, complete, delete, set reminders and toggle completed
+                  </Card.Text>
 
-                <a
-                  className="buttons btn btn-dark"
-                  href="https://github.com/NolanBeyer/Task-Tracker">
-                  <i className="fab fa-chrome"> Site</i>
-                </a>
-                <a
-                  className="buttons btn btn-dark"
-                  href="https://github.com/NolanBeyer/Task-Tracker">
-                  <i className="fab fa-github"> Code</i>
-                </a>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </div>
+                  <a
+                    className="buttons btn btn-dark"
+                    href="https://github.com/NolanBeyer/Task-Tracker">
+                    <i className="fab fa-chrome"> Site</i>
+                  </a>
+                  <a
+                    className="buttons btn btn-dark"
+                    href="https://github.com/NolanBeyer/Task-Tracker">
+                    <i className="fab fa-github"> Code</i>
+                  </a>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
     </Fragment>
   );
 };
