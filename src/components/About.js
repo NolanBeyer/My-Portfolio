3b62f730ea152162afa57074bc2ad40349f84fbe
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-11-17 14:25:41 -0800

    fixed typos in about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,51 +1,51 @@
-import React from "react";
-import "./About.css";
+import React from 'react'
+import './About.css'
 
 const About = () => {
   return (
-    <div className="about">
-      <div className="about__body">
-        <h1 className="about__title">About Me</h1>
-        <h4 className="about__description">
+    <div className='about'>
+      <div className='about__body'>
+        <h1 className='about__title'>About Me</h1>
+        <h4 className='about__description'>
           I've always been really interested in technology, but never thought
           about a career in web development until summer of 2019. I started
           doing some research and just trying to learn some of the basics in
-          HTML and CSS. I fell in love with the thought of making this a full
-          time career. I've spent countless hours over the last year and
-          although I still have a lot to learn I'm really excited for this next
-          step and am willing to learn additional technologies to fit the needs
-          of future employers.
+          HTML and CSS. The thought of making this a full time career got me
+          really excited. I've spent countless hours over the last year learning
+          and teaching myself. Although I still have a lot to learn I'm really
+          excited for this next step and am willing to learn additional
+          technologies to fit the needs of future employers.
         </h4>
-        <div className="work">
-          <h1 className="about__title">Experience</h1>
-          <div className="work__history">
-            <h2 className="work__title">Dev Mountain:</h2>
+        <div className='work'>
+          <h1 className='about__title'>Experience</h1>
+          <div className='work__history'>
+            <h2 className='work__title'>Dev Mountain:</h2>
             <h4>
               Immersive, 13-week web-development bootcamp for learning
               Javascript, HTML/CSS, React, Node, and PostgreSQL frameworks.
             </h4>
           </div>
-          <div className="work">
-            <h2 className="work__title">REdevly:</h2>
-            <div className="work__history">
-              <h4 className="work__description">
+          <div className='work'>
+            <h2 className='work__title'>REdevly:</h2>
+            <div className='work__history'>
+              <h4 className='work__description'>
                 Working part time for a startup primarily focusing on HTML,
-                CSS/SCSS along with learning the basics of angular.{" "}
+                CSS/SCSS along with learning the basics of angular.{' '}
               </h4>
             </div>
           </div>
         </div>
-        <div className="work">
-          <div className="work__history">
-            <h2 className="work__title">Graphic designer:</h2>
-            <h4 className="work__description">
-              Designed graphics for various buisnesess using adobe illustrator.
+        <div className='work'>
+          <div className='work__history'>
+            <h2 className='work__title'>Graphic designer:</h2>
+            <h4 className='work__description'>
+              Designed graphics for various businesses using adobe illustrator.
             </h4>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default About;
+export default About
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,50 +1,50 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
-import { useHistory } from "react-router";
-import "./NavBar.css";
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+import { useHistory } from 'react-router'
+import './NavBar.css'
 
 const NavBar = () => {
-  const history = useHistory();
+  const history = useHistory()
   return (
     <>
       <Navbar
-        className="NavBar"
-        fixed="top"
+        className='nav'
+        fixed='top'
         collapseOnSelect
-        expand="lg"
-        variant="dark">
+        expand='lg'
+        variant='dark'>
         <Navbar.Brand
-          className="home__icon"
-          style={{ cursor: "pointer" }}
+          className='home__icon'
+          style={{ cursor: 'pointer' }}
           onClick={() => {
-            history.push("/");
+            history.push('/')
           }}>
-          {" "}
-          <i class="home-icon fas fa-home"></i>
+          {' '}
+          <i class='home-icon fas fa-home'></i>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto"></Nav>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='mr-auto'></Nav>
 
           <Nav.Link
-            className="links"
+            className='links'
             onClick={() => {
-              history.push("/about");
+              history.push('/about')
             }}>
             About
           </Nav.Link>
 
           <Nav.Link
-            className="links"
+            className='links'
             onClick={() => {
-              history.push("/contact");
+              history.push('/contact')
             }}>
             Contact
           </Nav.Link>
         </Navbar.Collapse>
       </Navbar>
     </>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
